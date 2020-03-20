1de90ff3204251d7dd8c77cfb1fc45df2f6af2d1
davidyshka <david.komarovskiy@mail.ru>
2020-03-20 23:34:35 +0300

    js

--- js/common.js
+++ js/common.js
@@ -1,16 +1,32 @@
 function calc() {
     let num1 = document.getElementById('n1').value;
-    let num2 = 0.0012;
+    let link = document.getElementById('link__input').value;
+
+    let num2 = 0.00033;
     let result;
+
+    if (link === "") {
+        link = "Вставьте ссылку"
+
+    }
+
+
+
 
 
 
 
     result = num1 * num2
     document.getElementById('out').innerHTML = result.toFixed(4) * 1 + ' Руб.'
+    document.getElementById('result').innerHTML = "Итого: " + result
+    document.getElementById('view').innerHTML = "Количество просмотров: " + num1
+    document.getElementById('link__video').innerHTML = "Ссылка на видео: " + link
+
+
 
 
 }
+
 
 document.getElementById('header__opt').addEventListener('click', () => scrollToElement('#offers'))
 document.getElementById('header__price').addEventListener('click', () => scrollToElement('#calc'))
@@ -20,4 +36,6 @@ function scrollToElement(selector) {
     element && element.scrollIntoView({ behavior: "smooth" });;
 }
 
-
+$(document).ready(function () {
+    $('.pay__btn').magnificPopup();
+});
