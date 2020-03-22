9972019d98197b194076cf5a20b7986e365244c5
davidyshka <david.komarovskiy@mail.ru>
2020-03-23 00:37:32 +0300

    yea

--- app/js/common.js
+++ app/js/common.js
@@ -26,12 +26,44 @@ function calc() {
 
     result = num1 * num2
     document.getElementById('out').innerHTML = result.toFixed(4) * 1 + ' Руб.'
-    document.getElementById('result').innerHTML = "Итого: " + result + ' Руб.'
-    document.getElementById('view').innerHTML = "Количество просмотров: " + num1
-    document.getElementById('link__video').innerHTML = "Ссылка на видео: " + link
+    // document.getElementById('result').innerHTML = "Итого: " + result + ' Руб.'
+    document.getElementById('popup__view-input').value = num1
+    document.getElementById('popup__video-input').value = link
 
 
 }
+
+const qiwiBtn = document.getElementById('qiwi');
+const yandexBtn = document.getElementById('yandex');
+const perfectBtn = document.getElementById('perfect');
+const payeerBtn = document.getElementById('payeer');
+const btcBtn = document.getElementById('btc');
+const ethBtn = document.getElementById('eth');
+
+
+qiwiBtn.addEventListener('click', function () {
+    pay__value__img.src = './img/qiwi.png';
+});
+
+yandexBtn.addEventListener('click', function () {
+    pay__value__img.src = './img/yandex.png';
+});
+
+perfectBtn.addEventListener('click', function () {
+    pay__value__img.src = './img/perfect.png';
+});
+
+payeerBtn.addEventListener('click', function () {
+    pay__value__img.src = 'https://toplogos.ru/images/thumbs/preview-logo-payeer.png';
+});
+
+btcBtn.addEventListener('click', function () {
+    pay__value__img.src = 'https://toplogos.ru/images/thumbs/preview-logo-bitcoin.png';
+});
+
+ethBtn.addEventListener('click', function () {
+    pay__value__img.src = './img/eth.png';
+});
 
 
 
@@ -52,3 +84,7 @@ $(document).ready(function () {
     $('.pay__btn').magnificPopup();
 });
 
+$('.payments__link').click(function () {
+    $('.payments__link').not(this).removeClass('active');
+    $(this).toggleClass('active');
+});
