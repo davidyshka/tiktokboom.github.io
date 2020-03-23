1912249d61d65d0d31e35348e4bbc7030c9f38ba
davidyshka <david.komarovskiy@mail.ru>
2020-03-23 13:03:15 +0300

    popup

--- app/js/common.js
+++ app/js/common.js
@@ -25,13 +25,15 @@ function calc() {
 
 
     result = num1 * num2
-    document.getElementById('out').innerHTML = result.toFixed(4) * 1 + ' Руб.'
-    // document.getElementById('result').innerHTML = "Итого: " + result + ' Руб.'
+    document.getElementById('out').innerHTML = result.toFixed(3) * 1 + ' Руб.'
+    document.getElementById('result').innerHTML = ' ' + result + ' Руб.'
     document.getElementById('popup__view-input').value = num1
     document.getElementById('popup__video-input').value = link
 
 
 }
+
+
 
 const qiwiBtn = document.getElementById('qiwi');
 const yandexBtn = document.getElementById('yandex');
@@ -39,31 +41,66 @@ const perfectBtn = document.getElementById('perfect');
 const payeerBtn = document.getElementById('payeer');
 const btcBtn = document.getElementById('btc');
 const ethBtn = document.getElementById('eth');
+const payBtn = document.getElementById('pay__btn');
+const getRandom = Math.ceil(Math.random() * 100000 + 1);
 
+
+
+
+
+
+payBtn.addEventListener('click', function () {
+    personal__code.value = getRandom
+});
 
 qiwiBtn.addEventListener('click', function () {
-    pay__value__img.src = './img/qiwi.png';
+    pay__value__img.src = './img/qiwiMini.png';
+    wallet.value = '+79689192503'
+
 });
 
 yandexBtn.addEventListener('click', function () {
-    pay__value__img.src = './img/yandex.png';
+    pay__value__img.src = './img/yandexMini.png';
+    wallet.value = '410015036458557'
 });
 
 perfectBtn.addEventListener('click', function () {
-    pay__value__img.src = './img/perfect.png';
+    pay__value__img.src = './img/perfectMini.png';
+    wallet.value = 'U21982194'
 });
 
 payeerBtn.addEventListener('click', function () {
-    pay__value__img.src = 'https://toplogos.ru/images/thumbs/preview-logo-payeer.png';
+    pay__value__img.src = './img/payeerMini.png';
+    wallet.value = 'P1023488452'
 });
 
 btcBtn.addEventListener('click', function () {
-    pay__value__img.src = 'https://toplogos.ru/images/thumbs/preview-logo-bitcoin.png';
+    pay__value__img.src = './img/btcMini.jpg';
+    wallet.value = '1EWqJBqR9DiD7CzZWrDcWzaSLidAHt5wZa'
 });
 
 ethBtn.addEventListener('click', function () {
     pay__value__img.src = './img/eth.png';
+    wallet.value = '0xDe37f4F0b595f948F8D22Ae0806562cc067C2CC4'
 });
+
+
+function copyWallet() {
+
+    let copyText = document.getElementById("wallet");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
+
+function copyCode() {
+
+    let copyText = document.getElementById("personal__code");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+}
+
 
 
 
